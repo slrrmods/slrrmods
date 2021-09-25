3c07eb4f15a872fa4a8ab1df4e74f6a196ca3845
Adnan Pedroso <adnan_silva54@hotmail.com>
2021-09-25 20:23:44 -0300

    start login page

--- components/SubHeader/index.js
+++ components/SubHeader/index.js
@@ -23,12 +23,14 @@ export default function SubHeader() {
 	const classes = useStyles();
 
 	return (
-		<div className={classes.root}>
-			<Carousel className={classes.carousel} navButtonsAlwaysVisible={true}>
-				<img src="http://upload.vstanced.com/images/2019/01/11/fea5e4d3a76c537ef7085ca6ef8166a0.jpg" />
-				<img src="http://upload.vstanced.com/images/2019/02/05/c04d2a711faa5241cf08ecfb579fd73a.jpg" />
-				<img src="http://upload.vstanced.com/images/2019/06/18/1e02aee72aa3906dd65849fcc55ee8ae.jpg" />
-			</Carousel>
-		</div>
+		<Paper>
+			<div className={classes.root}>
+				<Carousel className={classes.carousel} navButtonsAlwaysVisible={true}>
+					<img src="http://upload.vstanced.com/images/2019/01/11/fea5e4d3a76c537ef7085ca6ef8166a0.jpg" />
+					<img src="http://upload.vstanced.com/images/2019/02/05/c04d2a711faa5241cf08ecfb579fd73a.jpg" />
+					<img src="http://upload.vstanced.com/images/2019/06/18/1e02aee72aa3906dd65849fcc55ee8ae.jpg" />
+				</Carousel>
+			</div>
+		</Paper>
 	);
 }
--- pages/login.js
+++ pages/login.js
@@ -1,3 +1,19 @@
+import { Stack, Paper, Container } from "@material-ui/core";
+
 export default function LoginPage() {
-	return <div>aqui é o login</div>;
+	return (
+		<Container>
+			<Stack
+				direction="column"
+				justifyContent="center"
+				alignItems="center"
+				spacing={3}>
+				<Paper>Logo</Paper>
+
+				<Paper>Login</Paper>
+
+				<Paper>Create account</Paper>
+			</Stack>
+		</Container>
+	);
 }
