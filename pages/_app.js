39b3c48a86d040323b95d66f4fb646c8b4879299
Adnan Pedroso <adnan_silva54@hotmail.com>
2022-10-16 16:19:40 -0300

    fresh install

--- pages/_app.js
+++ pages/_app.js
@@ -1,39 +1,7 @@
-import React, { useEffect } from "react";
-import Head from "next/head";
-import CssBaseline from "@material-ui/core/CssBaseline";
+import '../styles/globals.css'
 
-import { ThemeProvider } from "@material-ui/core/styles";
-import { UserProvider } from "../contexts/UserContext";
-import { DrawerProvider } from "../contexts/DrawerContext";
-
-import theme from "../styles/themes";
-
-export default function MyApp({ Component, pageProps }) {
-	useEffect(() => {
-		const jssStyles = document.querySelector("#jss-server-side");
-
-		if (jssStyles) jssStyles.parentElement.removeChild(jssStyles);
-	}, []);
-
-	return (
-		<>
-			<Head>
-				<title>SLRR Mods</title>
-				<link rel="icon" href="/favicon.ico" />
-				<meta
-					name="viewport"
-					content="minimum-scale=1, initial-scale=1, width=device-width"
-				/>
-			</Head>
-
-			<UserProvider>
-				<DrawerProvider>
-					<ThemeProvider theme={theme}>
-						<CssBaseline />
-						<Component {...pageProps} />
-					</ThemeProvider>
-				</DrawerProvider>
-			</UserProvider>
-		</>
-	);
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
+
+export default MyApp
