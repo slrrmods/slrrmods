89bbcb7a6a269d273fdbf16c7f96f73af0635dd7
Adnan Pedroso <adnan_silva54@hotmail.com>
2022-10-22 04:31:47 -0300

    update 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,21 +1,26 @@
-import { useStyles } from "../styles/404.styles";
-import { Title, Divider, Stack, Text, Anchor } from "@mantine/core";
 import Link from "next/link";
+import { Title, Text, Button, Container, Group } from "@mantine/core";
+import { useStyles } from "../styles/404.styles";
 
 export default function NotFound() {
 	const { classes } = useStyles();
+
 	return (
-		<div className={classes.root}>
-			<div className={classes.content}>
-				<Title order={1}>404</Title>
-				<Divider orientation="vertical" />
-				<Stack spacing={"sm"}>
-					<Text>This page could not be found.</Text>
-					<Link href="/" passHref>
-						<Anchor component="a">Go back to home page.</Anchor>
-					</Link>
-				</Stack>
-			</div>
-		</div>
+		<Container className={classes.root}>
+			<div className={classes.label}>404</div>
+
+			<Title className={classes.title}>You have found a secret place.</Title>
+
+			<Text className={classes.description} color="dimmed" size="lg">
+				Unfortunately, this is only a 404 page. You may have mistyped the
+				address, or the page has been moved to another URL.
+			</Text>
+
+			<Link href="/" passHref>
+				<Button component="a" variant="subtle" size="md">
+					Take me back to home page
+				</Button>
+			</Link>
+		</Container>
 	);
 }
--- styles/404.styles.js
+++ styles/404.styles.js
@@ -1,15 +1,33 @@
 import { createStyles } from "@mantine/core";
+import { themeHelpers } from "../utils";
 
 export const useStyles = createStyles((theme) => ({
 	root: {
 		margin: "auto",
+		display: "flex",
+		flexDirection: "column",
+		placeItems: "center",
+		gap: theme.spacing.xl,
 	},
 
-	content: {
-		display: "flex",
-		alignItems: "center",
-		justifyContent: "center",
-		width: "100%",
-		gap: theme.spacing.lg,
+	label: {
+		lineHeight: "1",
+		fontWeight: 900,
+		fontSize: 220,
+		color: themeHelpers.getFromConditional(
+			theme,
+			theme.colors.dark[4],
+			theme.colors.gray[2]
+		),
+	},
+
+	title: {
+		fontWeight: 900,
+		fontSize: 38,
+	},
+
+	description: {
+		maxWidth: 500,
+		textAlign: "center",
 	},
 }));
