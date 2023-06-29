a923ebcd876f1e763ef80956be40748f3d564d36
Adnan Pedroso <adnan_silva54@hotmail.com>
2023-06-29 06:05:40 -0300

    update env variables names

--- services/supabase-client.js
+++ services/supabase-client.js
@@ -13,14 +13,14 @@ export function createClient() {
 }
 
 function createForClient() {
-	const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-	const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+	const supabaseUrl = process.env.SUPABASE_URL;
+	const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
 	return createSupabaseClient(supabaseUrl, supabaseKey);
 }
 
 function createForServer() {
-	const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+	const supabaseUrl = process.env.SUPABASE_URL;
 	const supabaseKey = process.env.SUPABASE_SERVICE_KEY;
 
 	return createSupabaseClient(supabaseUrl, supabaseKey);
