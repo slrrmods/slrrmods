68ec3e704ca96ad228c4dd295c7e99b23ffc6629
Adnan Pedroso <adnan_silva54@hotmail.com>
2023-06-29 16:53:31 -0300

    fix useState in sign pages

--- pages/users/signUp.js
+++ pages/users/signUp.js
@@ -1,18 +1,7 @@
 import { Card, Center, Title } from "@mantine/core";
-import { useRouter } from "next/router";
 import { SignUpForm } from "../../components/Forms";
-import { useUserContext } from "../../contexts";
 
 export default function SignUp() {
-	const router = useRouter();
-	const { user } = useUserContext();
-
-	useEffect(() => {
-		if (user) router.push("/");
-	}, [user, router]);
-
-	if (user) return <></>;
-
 	return (
 		<Center>
 			<Card
