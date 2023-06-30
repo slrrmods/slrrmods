70a40831990902a55dd75dc79e01527527a7b444
Adnan Pedroso <adnan_silva54@hotmail.com>
2023-06-30 04:16:04 -0300

    try move geoip data location 2

--- next.config.js
+++ next.config.js
@@ -67,6 +67,13 @@ const nextConfig = {
 			},
 		];
 	},
+	webpack: (config, { isServer }) => {
+		if (!isServer) {
+			config.resolve.fallback.fs = false;
+		}
+
+		return config;
+	},
 };
 
 module.exports = withAxiom({
