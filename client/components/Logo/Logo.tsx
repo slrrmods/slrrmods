163469de5c4a26dab5ed91d4e8099b5748205aad
Adnan Pedroso <adnan_silva54@hotmail.com>
2023-07-15 17:39:23 -0300

    update logo component

--- client/components/Logo/Logo.tsx
+++ client/components/Logo/Logo.tsx
@@ -1,10 +1,46 @@
 import { Link } from "@client/components";
-import { Title } from "@mantine/core";
+import { Title, useMantineTheme } from "@mantine/core";
 
-export function Logo() {
-	return (
-		<Link href="/">
-			<Title order={3}>SLRR MODS</Title>
-		</Link>
+type Variants = "light" | "dark" | "colored" | "inherit";
+type Sizes = "xs" | "sm" | "md" | "lg" | "xl";
+
+export type LogoProps = {
+	asAnchor?: boolean;
+	variant?: Variants;
+	size?: Sizes;
+};
+
+export function Logo({
+	asAnchor = true,
+	variant = "inherit",
+	size = "md",
+}: LogoProps) {
+	const theme = useMantineTheme();
+
+	function getOrder(size: Sizes) {
+		if (size === "xs") return 5;
+		if (size === "sm") return 4;
+		if (size === "md") return 3;
+		if (size === "lg") return 2;
+		if (size === "xl") return 1;
+	}
+
+	function getColor(variant: Variants) {
+		if (variant === "inherit") return undefined;
+		if (variant === "light") return "white";
+		if (variant === "dark") return "black";
+		if (variant === "colored") return theme.fn.primaryColor();
+	}
+
+	const titleComponent = (
+		<Title order={getOrder(size)} color={getColor(variant)}>
+			SLRR MODS
+		</Title>
 	);
+
+	if (asAnchor) {
+		return <Link href="/">{titleComponent}</Link>;
+	}
+
+	return titleComponent;
 }
